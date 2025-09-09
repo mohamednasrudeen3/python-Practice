efe6db0675f043cfd006f1035a2fb9a572d11630
mohamednasrudeen3 <mohamednasrudeen21@gmail.com>
2025-09-09 19:25:07 +0530

    Initial Commit

---
+++ String.py
@@ -0,0 +1,28 @@
+message="Nasrudeen"
+quote='"as far back as"'
+team="Ln's team"
+
+name = "mohamed nasrudeen"
+
+# function, method - performs a specified task
+# function always called directly
+# method always invoke. called by .-> invoking a method
+print(message.upper())
+print(message.lower())
+print(name.title())
+
+# if we title first letter would be Capital
+
+print("hello \n world")# escape sequence-> \
+print("moideen\tabdul")# escape sequence-? \
+
+# String Methods
+print(len(message))
+strmethod = "nandini"
+print(strmethod.find("n"))
+print(strmethod.count('n'))
+person1, age, height = "Nasrudeen", 67, 186
+print(height)
+like = dislike = 1500
+print(dislike)
+print(like)
---
+++ first.py
@@ -0,0 +1,15 @@
+# python is dynamically Typed Language
+# We dont have to specify data Types
+# name = "Nasrujhaug%65#"
+# print("Hi "+name)
+'''
+Multi Line
+Comment
+Valid for Variables Name
+Valid - name,first_name,name_123, _name(onle "_" is valid for special Character)
+Not Valid - name#, name$, 1name, print
+'''
+number = 1223
+num= 4.6777
+child = True
+print(child)

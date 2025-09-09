9632c7b8e470588812b166e3549d687c85dade71
mohamednasrudeen3 <mohamednasrudeen21@gmail.com>
2025-09-09 21:42:30 +0530

    Type Cast

---
+++ TypeCasting.py
@@ -0,0 +1,19 @@
+otp = 754709
+
+print("hello, your Otp is " + str(otp))
+
+name = "Anand"
+period = 15
+year = 2021
+
+print("Dear " + name)
+print(f"you have {period} days of leave balance for this year {year}")
+
+
+name = (input("what is your name: "))
+height = float(input("Enter your Height: "))
+height_inches ="{:.2f}".format(height/2.54)
+print(type(height))
+print(f"hi {name}")
+print(f"your height is {height}cm")
+print(f"your height is {height_inches}inchs")

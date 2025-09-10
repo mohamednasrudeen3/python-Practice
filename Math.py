38c6530fd729b16eacbe83b694413bf443ed6690
mohamednasrudeen3 <mohamednasrudeen21@gmail.com>
2025-09-10 09:00:56 +0530

    Math fuctions

---
+++ Math.py
@@ -0,0 +1,21 @@
+a= 8.78
+print(round(a))
+
+a=-5
+print(abs(a))
+b = 10
+print(pow(b,2))
+print(b**3)
+
+c= 15
+d =12
+
+print(max(c,d))
+print(min(b,c,d))
+
+import math
+
+e = 8.91
+
+print(math.ceil(e))
+print(math.floor(e))
